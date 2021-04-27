6873a684a96524d7ef26cd9584245d4349d57723
472529 <472529@chrome.escg.ac.uk>
2021-04-27 13:00:57 +0100

    Switched to URP instead of HDRP. Made the lighting darker though tweaking is still nessacery. Added health packs into the ammo spawning while i figure out how to make them spawn on the walls.

--- FMP/Assets/AmmoSpawner.cs
+++ FMP/Assets/AmmoSpawner.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class AmmoSpawner : MonoBehaviour
 {
     Object ammoBox;
+    Object healthPack;
     // Start is called before the first frame update
     void Start()
     {
         ammoBox = Resources.Load("AmmoBox");
+        healthPack = Resources.Load("HealthPack");
         Spawner();
     }
 
     // Update is called once per frame
     void Spawner()
     {
-        float random = Random.Range(-1, 4);
+        float random = Random.Range(-1, 6);
         if (random == 2)
         {
             Instantiate(ammoBox, transform.position, transform.rotation);
+        }
+        else if(random == 3)
+        {
+            Instantiate(healthPack, transform.position, transform.rotation);
         }
         else
         {
