98e3ef21bc75222ded438bfdca547f23640eea1a
472529 <472529@chrome.escg.ac.uk>
2021-04-28 15:24:06 +0100

    added volumetric lights to the game and spawner for the lights so the game isnt so dark but still adds to the moody atmosphere, added a fire sound to the robot enemies so you can hear them coming up behind you and makes more sense with them flying using a jetpack thing.

---
+++ FMP/Assets/Scripts/LightSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSpawner : MonoBehaviour
+{
+    Object light;
+
+    private void Start()
+    {
+        light = Resources.Load("Lamp");
+        Spawner();
+    }
+
+    void Spawner()
+    {
+        Instantiate(light, transform.position, transform.rotation);
+
+    }
+}
