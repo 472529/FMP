38ae1a5cda4ffe6f5adf3837a16b463824b2a52c
472529 <472529@chrome.escg.ac.uk>
2021-04-29 16:00:04 +0100

    Added volumetric lighting to chambers but doesnt look good for the impact in performance so is disabled for now. Some changes to main menu and exit script in attempt to fix a bug previously fixed but has resurfaced. Bullets also now go through walls even with a collider that isnt a trigger will be trying to fix this as it is detrimental to gameplay.

--- FMP/Assets/Scripts/Exit.cs
+++ FMP/Assets/Scripts/Exit.cs
@@ -5,11 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
+    Player p;
+    private void Start()
+    {
+        p = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals("Player"))
         {
+            p.isLoadScene = true;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
         }
     }
 }
